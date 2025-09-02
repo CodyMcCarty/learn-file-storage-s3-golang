1df0412c052e9f2ad862921bd1abd809532210be
Cody McCarty <codyjmccarty@gmail.com>
2025-09-02 14:58:34 -0500

    c1.l7 using the filesystem

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -67,14 +68,39 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		mediaType: mediaType,
 	}
 
-	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
-	imageData := base64.StdEncoding.EncodeToString(data)
-	url = fmt.Sprintf("data:%v;base64,%s", mediaType, imageData)
+	// Instead of encoding to base64, update the handler to save the bytes to a file at the path /assets/<videoID>.<file_extension>.
+
+	filename := videoID.String() + ".png"
+	diskPath := filepath.Join(cfg.assetsRoot, filename)
+
+	f, err := os.Create(diskPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating file", err)
+		return
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(f, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error writing file", err)
+		return
+	}
+
+	url := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, filename)
 	video.ThumbnailURL = &url
+
+	//url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	//base64Encoded := base64.StdEncoding.EncodeToString(data)
+	//url = fmt.Sprintf("data:%v;base64,%s", mediaType, base64Encoded)
+	//video.ThumbnailURL = &url
+
+	// Use the Content-Type header to determine the file extension.
+	// Use the videoID to create a unique file path. filepath.Join and cfg.assetsRoot will be helpful here.
+	// Use os.Create to create the new file
+	// Copy the contents from the multipart.File to the new file on disk using io.Copy
 
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
-		delete(videoThumbnails, videoID)
+		//delete(videoThumbnails, videoID)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
 		return
 	}
